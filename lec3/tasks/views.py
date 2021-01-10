496e1610708e0c643962d2fe93780e8441a0c261
Youngsuk Kim <1990youngsuk@gmail.com>
2021-01-10 15:51:33 +0900

    global task

--- lec3/tasks/views.py
+++ lec3/tasks/views.py
@@ -1,14 +1,15 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
-items = ["foo", "bar", "baz"]
+items = []
 
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 
 def index(request):
@@ -23,6 +24,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             items.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
